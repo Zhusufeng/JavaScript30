f19ada9e28e27de76b7b987a1e5beb3c1cdbfc39
Lisa Gee <zhusufeng@gmail.com>
2017-12-06 10:39:26 -0800

    Add timeupdate listener & fill in handleProgress

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -37,10 +37,20 @@ function handleRangeUpdate() {
   video[this.name] = this.value;
 }
 
+function handleProgress() {
+  // Get percentage
+  const percent = (video.currentTime / video.duration) * 100;
+
+  // Update flex-basis percentage
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
 /* Hook up the event listeners */
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
 toggle.addEventListener('click', togglePlay);
 skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
