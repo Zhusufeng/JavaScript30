d9509929d153bda51f1888b8dd0f31e99fdbb125
Lisa Gee <zhusufeng@gmail.com>
2017-12-06 11:02:44 -0800

    Add eventListener if mousedown, run scrub

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -65,5 +65,14 @@ skipButtons.forEach(button => button.addEventListener('click', skip));
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
 
+let mousedown = false;
 progress.addEventListener('click', scrub);
-progress.addEventListener('mousemove', scrub);
+// checks if mousedown is true, run scrub func
+progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
+// progress.addEventListener('mousemove', (e) => {
+//   if (mousedown) {
+//     scrub(e);
+//   }
+// });
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
